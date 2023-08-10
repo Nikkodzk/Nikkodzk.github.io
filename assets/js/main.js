90c15e978b7b7629695eba5c9221480fcfce6941
Nicolas <60112394+Nikkodzk@users.noreply.github.com>
2023-08-10 14:41:10 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -240,3 +240,96 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+
+/**
+* Template Name: ComingSoon
+* Updated: Jul 27 2023 with Bootstrap v5.3.1
+* Template URL: https://bootstrapmade.com/comingsoon-free-html-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Countdown timer
+   */
+  let countdown = select('.countdown');
+  let over = select('.regresiva');
+  const output = countdown.innerHTML;
+
+  const countDownDate = function() {
+    let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
+
+    let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    let tt = 'FIEsTA'
+
+    countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
+
+    // SI LA FECHA YA LLEGO
+    if ((Math.floor(timeleft / (1000 * 60 * 60 * 24))) < 0) {  
+    //countdown.innerHTML = output.replace('%d', 0).replace('%h', 0).replace('%m', 0).replace('%s', 0); // funaiona ok
+
+    //over.innerHTML = '' // oculta la seccion OK
+
+    }
+  }
+
+  countDownDate();
+  setInterval(countDownDate, 1000);
+  
+
+})()
